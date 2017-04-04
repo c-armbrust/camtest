11078d19325011be8b05caa303c5dbcbca9bed00
Christian Armbrust <c.armbrust@hs-mannheim.de>
2017-04-04 07:32:35 +0000

    terminate_on_error function

--- camtest.cpp
+++ camtest.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <stdio.h>
 #include <ueye.h>
 #include <ueye_deprecated.h>
+
+void terminate_on_error(HIDS);
 
 int main()
 {
@@ -11,21 +14,21 @@ int main()
 	ret = is_InitCamera(&h, NULL);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_InitCamera error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	CAMINFO camerainfo;
 	ret = is_GetCameraInfo(h, &camerainfo);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_GetCameraInfo error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	SENSORINFO sensorinfo;
 	ret = is_GetSensorInfo(h, &sensorinfo);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_GetSensorInfo error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	
 	std::cout << "Sensor model " << sensorinfo.strSensorName << ". Camera serial no " << camerainfo.SerNo << ".\n";
@@ -40,21 +43,21 @@ int main()
 	ret = is_SetColorMode(h, IS_SET_CM_BAYER);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SetColorMode error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	INT nMinPixelClock=0, nMaxPixelClock=0;
 	ret = is_GetPixelClockRange(h, &nMinPixelClock, &nMaxPixelClock);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_GetPixelClockRange error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	std::cout << "Pixelclock range " <<  nMinPixelClock << "-" << nMaxPixelClock << "."  << std::endl;
 	std::cout << "Set Pixelclock to " << nMinPixelClock << "." <<  std::endl;
 	ret = is_SetPixelClock(h, nMinPixelClock);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SetPixelClock error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	
 	INT bpp = 8;
@@ -64,28 +67,28 @@ int main()
 	ret = is_AllocImageMem(h, width, height, bpp, &pmem, &id);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_AllocImageMem error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	std::cout << "Set image memory." << std::endl;
 	ret = is_SetImageMem(h, pmem, id);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SetImageMem error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	std::cout << "Set image size." << std::endl;
 	ret = is_SetImageSize(h, width, height);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SetImageSize error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	std::cout << "Set image position." << std::endl;
 	ret = is_SetImagePos(h, 0, 0);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SetImagePos error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}	
 
 	double dblMinFrameTime = 0.0, dblMaxFrameTime = 0.0, dblFrameTimeInterval = 0.0;
@@ -93,14 +96,14 @@ int main()
 	ret = is_GetFrameTimeRange(h, &dblMinFrameTime, &dblMaxFrameTime, &dblFrameTimeInterval);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_GetFrameTimeRange error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	std::cout << "Frametime range " << dblMinFrameTime << "-" << dblMaxFrameTime << "." << std::endl;
 
 	ret = is_SetFrameRate(h, IS_GET_FRAMERATE, & dblframerate);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SetFrameRate error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	std::cout << "Applied Framerate " << dblframerate << "." << std::endl;
 
@@ -108,14 +111,14 @@ int main()
 	ret = is_FreezeVideo(h, sleeptime);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_FreezeVideo error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
 	double dblFps = 0.0;
 	ret = is_GetFramesPerSecond(h, &dblFps);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_GetFramesPerSecond error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	std::cout << "Fps " << dblFps << "." << std::endl;
 	
@@ -124,22 +127,39 @@ int main()
 	ret = is_SaveImage(h, filename);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_SaveImage error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 	
 	ret = is_FreeImageMem(h, pmem, id);
 	if(ret != IS_SUCCESS) {
 		std::cout << "is_FreeImageMem error." << std::endl;
-		return 1;
+		terminate_on_error(h);
 	}
 
-	std::cout << "Exit board." << std::endl;
-	ret = is_ExitBoard(h);
+	std::cout << "Exit camera." << std::endl;
+	ret = is_ExitCamera(h);
 	if(ret != IS_SUCCESS) {
-		std::cout << "is_ExitBoard error." << std::endl;
-		return 1;
+		std::cout << "is_ExitCamera error." << std::endl;
+		terminate_on_error(h);
 	}
 
     return 0;
+}
+
+
+void terminate_on_error(HIDS h)
+{
+	INT pErr=0; // Errorcode
+	INT ret;
+	IS_CHAR* ppcErr; // Error message
+	is_GetError(h, &pErr, &ppcErr);
+	std::cout << "Terminate on error." << std::endl;
+	std::cout << "Error code: " << pErr << ".\n";
+	printf("Error message: %s\n", ppcErr);
+	ret = is_ExitCamera(h);
+	if(ret != IS_SUCCESS) {
+		std::cout << "is_ExitCamera error." << std::endl;
+	}
+	exit(1);
 }
 
